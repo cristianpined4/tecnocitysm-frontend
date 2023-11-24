35391d5a458cf6d9200ba3cb04059e8ac364a0ba
lm21009UES <lm21009@ues.edu.sv>
2023-11-24 17:50:25 -0600

    categorias en funcion de la API

--- public/js/brands.js
+++ public/js/brands.js
@@ -10,3 +10,28 @@ document.addEventListener("DOMContentLoaded", function() {
         inputTexto.style.display = inputTexto.style.display === "none" ? "block" : "none";
     });
 });
+
+const seccionMarcas = document.getElementById("sectionMarcas");
+const obtenerMarcas = async () => {
+    const respuesta = await fetch("http://localhost:8000/api/marcas/get-marcas", {
+        method: "GET",
+    });
+    const datos = await respuesta.json();
+    let marca = "";
+    datos.marcas.forEach(element => {
+        marca+=`<div class="itemsProduct">
+        <div class="imgProducts">
+                <img src="${element.images[0].url}" alt="marcas">
+            </div>
+            <div class="infoProducto">
+            <h4>${element.nombre}</h4>
+
+            </div>
+
+    </div>`;
+    });
+    seccionMarcas.innerHTML= marca;
+    // selectMarcas.innerHTML = "";
+};
+
+obtenerMarcas();
